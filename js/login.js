a42e808e377a8968c193acb35cac1a8b20b6db68
Miguel Servera <miguelservera99@gmail.com>
2022-04-13 11:54:43 +0200

    Added style

--- js/login.js
+++ js/login.js
@@ -1,6 +1,28 @@
+const url = ""
 $(function () {
     $("#register").click(function (e) { 
         e.preventDefault();
-        
+        verifyEmail();
+        registerUser();
     });
 });
+
+function verifyEmail(){
+    
+}
+
+function registerUser(){
+    $.ajax({
+        type: "POST",
+        url: url+"/registerUser",
+        data: {
+            "email": $("#registerEmail").val(),
+            "password": $("#registerPassword").val(),
+            "userName": $("#registerUsername").val()
+        },
+        dataType: "dataType",
+        success: function (response) {
+            
+        }
+    });
+}
