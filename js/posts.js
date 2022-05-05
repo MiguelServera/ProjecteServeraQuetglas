206052ec887aba50b670378f661d879167f05a43
Miguel Servera <miguelservera@paucasesnovescifp.cat>
2022-05-05 18:35:10 +0200

    Seguit amb crear dinámicament l'index

--- js/posts.js
+++ js/posts.js
@@ -1,48 +1,29 @@
 const url = "http://stm.projectebaleart.com/public/api"
 let user = "";
 
-/*(Cookies.get("user") != undefined)
+(Cookies.get("user") != undefined)
 ?user = Cookies.get("user")
-:window.location.href="http://localhost/SpotTheMusic/ProjecteServeraQuetglas/";*/
+:window.location.href="http://localhost/SpotTheMusic/ProjecteServeraQuetglas/";
 
 $(function () {
+    $(".user").text(user);
     getPosts();
     randomSuggests();
+
     $(".sendPost").click(function (e) { 
         e.preventDefault();
         insertPost();
     });
 
-    let availableTags = [
-        "ActionScript",
-        "AppleScript",
-        "Asp",
-        "BASIC",
-        "C",
-        "C ++",
-        "Clojure",
-        "COBOL",
-        "ColdFusion",
-        "Erlang",
-        "Fortran",
-        "Maravilloso",
-        "Haskell",
-        "Java",
-        "JavaScript",
-        "Lisp",
-        "Perl",
-        "PHP",
-        "Python",
-        "Ruby",
-        "Scala",
-        "Esquema"
+    let availableFollowers = [
+
       ];
 
       $( "#searchFollower" ).autocomplete({
         open : function(){
             $(".ui-autocomplete:visible").css({top:"+=5",left:"-=6"});
         },
-        source: availableTags
+        source: availableFollowers
       });
 
       jQuery.ui.autocomplete.prototype._resizeMenu = function () {
@@ -59,7 +40,7 @@ $(function () {
 function getPosts() {
     $.ajax({
         type: "GET",
-        url: url+"/posts/8",
+        url: url+"/posts/"+user,
         dataType: "json",
         success: function (response) {
             response.forEach(element => {
@@ -91,7 +72,7 @@ function insertPost(){
         url: url+"/posts",
         data: {
             "text":$("#textPost").text(),
-            "user":8
+            "user":user
         },
         dataType: "json",
         success: function (response) {
@@ -122,8 +103,23 @@ function followUser(parent){
         type: "POST",
         url: url+"/followers",
         data: {
-            "userFollows":8,
-            "userFollowed":parent
+            "userFollows":2,
+            "userFollowed":8
+        },
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+        }
+    });
+}
+
+function deleteFollowUser(parent){
+    $.ajax({
+        type: "DELETE",
+        url: url+"/followers",
+        data: {
+            "userFollows":2,
+            "userFollowed":8
         },
         dataType: "json",
         success: function (response) {
@@ -153,6 +149,36 @@ function randomSuggests(){
                     "</div>"+
                 "</div>"+
                 "<button type='button' class='btn btn-primary followButton'>Follow</button></div>");
+            });
+            $(".followButton").click(function (e) { 
+                let parent = $(this).parent().attr("id");
+                followUser(parent);
+            });
+        }
+    });
+}
+
+function getFollowersMessages(){
+    $.ajax({
+        type: "GET",
+        url: url+"/followers/"+user,
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            response.forEach(element => {
+                $("#followersMessages").append("<div class='followerChat'>"+
+                "<div class='d-flex'>"+
+                  "<img class='userIcon mr-2' src='img/userIcon.png' alt='' />"+
+                  "<div class='userInfo mw-100'>"+
+                    "<div class='name'>"+
+                      "<span>"+element['username']+"</span>"+
+                    "</div>"+
+                    "<div class='message'>"+
+                      "<span>Oye habla conmigo vamos!</span>"+
+                    "</div>"+
+                  "</div>"+
+                "</div>"+
+              "</div>'");
             });
             $(".followButton").click(function (e) { 
                 let parent = $(this).parent().attr("id");
